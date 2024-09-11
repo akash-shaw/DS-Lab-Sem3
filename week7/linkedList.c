457fa4d9a351a68152f4c803f58f8b17f8af5d6a
Akash Shaw <136084773+akash-shaw@users.noreply.github.com>
2024-09-11 14:15:08 +0530

    UPDATE: Sort and Insert in Sorted

--- week7/linkedList.c
+++ week7/linkedList.c
@@ -1,235 +1,179 @@
-// https://drive.google.com/drive/folders/11T1i8J8wSxqTFnska4Jh4HQ5xdhB3u23
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-
-
-struct node
-{
+struct node {
     int info;
     struct node *link;
-}*start;
+} *start = NULL;
 
-//int create_list(int),addatbeg(int),addafter(int,int),del(int),display(),count(),rev(),search(int);
-
-//TODO: Check it once
-void insertAtPos(int data, int pos){
-    if(start==NULL){
-        addAtBeg(data);
-    }
-    struct node *tmp,*q;
+void addAtEnd(int data) {
+    struct node *q, *tmp;
     tmp = (struct node *)malloc(sizeof(struct node));
-
     tmp->info = data;
-    tmp->link=NULL;
+    tmp->link = NULL;
 
-    q = start;
-    int i=0
-    while(i<pos){
-        i++;
-        q = q->link;
-    }
-    temp->link=q->link;
-    q->link=temp;
-}
-void addAtEnd (int data){
-    struct node *q,*tmp;
-    tmp=(struct node *) malloc(sizeof(struct node));
-
-    tmp->info=data;
-    tmp->link=NULL;
-
-    if(start==NULL){
+    if (start == NULL) {
         start = tmp;
-    }
-    else{
-        q=start;
-        while(q->link != NULL){
+    } else {
+        q = start;
+        while (q->link != NULL) {
             q = q->link;
         }
-        q->link=tmp;
+        q->link = tmp;
     }
 }
 
-void addAtBeg(int data){
-    struct node *tmp;
-
-    tmp =(struct node *)malloc(sizeof(struct node));
-
-    tmp->info =data;
-    tmp->link=NULL;
-
-    if(start==NULL)
-        start=tmp;
-    else{
-        tmp->link =start;
-        start=tmp;
-    }
-}
-
-int del(int data)
-{
-	int found=0;
-	struct node *tmp,*q,*pre;
-	if(start->info == data)
-	{
-		tmp=start;
-		start=start->link;  /*First element deleted*/
-		free(tmp);
-		found=1;
-	}
-	else
-	{
-		found=0;
-		q=start;
-		while(q->link!=NULL)
-		{
-
-			if(q->info==data)     /*Element deleted in between*/
-			{
-				found=1;
-				pre->link=q->link;
-				tmp=q;
-				free(tmp);
-				break;
-			}
-			pre=q;
-			q=q->link;
-
-        }
-		if(q->info==data)    /*Last element deleted*/
-		{
-			tmp=q;
-			free(tmp);
-			pre->link=NULL;
-			found=1;
-		}
-	}
-	if(found==0){
-		printf("Element %d not found\n",data);
-	}
-
-}
-
-void delAltNode(){
-    struct node *tmp;
-
-    // tmp = (struct node *) malloc(sizeof(struct node));
-
-    tmp = start;
-
-    while(tmp->link!=NULL && tmp->link->link!=NULL){
-        tmp->link = (tmp->link)->link;
-        tmp=tmp->link;
-    }
-    tmp->link=NULL;
-}
-
-void insertInSorted(int data){
-    if(data<start->info){
-        addAtBeg(data);
-        return;
-    }
-    struct node *q,*tmp;
-    
-    q=start;
-    while(q->link!=NULL){
-        if(q->link->info>=data){
-            tmp=(struct node*)malloc(sizeof(struct node));
-            tmp->info = data;
-            tmp->link=q->link;
-            q->link=tmp;
-            return;
-        }
-        q = q->link;
-    }
-    addAtEnd(data);
-}
-
-void display(){
+void addAtBeg(int data) {
     struct node *tmp;
     tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    tmp->link = start;
+    start = tmp;
+}
 
-    if(start==NULL){
+int del(int data) {
+    int found = 0;
+    struct node *tmp, *q, *pre;
+    if (start == NULL) return 0;
+
+    if (start->info == data) {
+        tmp = start;
+        start = start->link;
+        free(tmp);
+        found = 1;
+    } else {
+        found = 0;
+        q = start;
+        while (q->link != NULL) {
+            if (q->link->info == data) {
+                found = 1;
+                tmp = q->link;
+                q->link = tmp->link;
+                free(tmp);
+                break;
+            }
+            q = q->link;
+        }
+    }
+    return found;
+}
+
+void display() {
+    struct node *tmp;
+    if (start == NULL) {
         printf("Empty list\n");
         return;
     }
-    tmp=start;
-    while(tmp!=NULL){
-        printf("%d ",tmp->info);
+    tmp = start;
+    while (tmp != NULL) {
+        printf("%d ", tmp->info);
+        tmp = tmp->link;
+    }
+    printf("\n");
+}
+
+void delAltNode() {
+    struct node *tmp = start;
+    while (tmp != NULL && tmp->link != NULL) {
+        struct node *toDelete = tmp->link;
+        tmp->link = toDelete->link;
+        free(toDelete);
         tmp = tmp->link;
     }
 }
 
-void rev()
-{
-	struct node *p1,*p2,*p3;
-	if(start->link==NULL)    /*only one element*/
-        return;
-
-	p1=start;
-	p2=p1->link;
-	p3=p2->link;
-	p1->link=NULL;
-	p2->link=p1;
-	while(p3!=NULL)
-	{
-		p1=p2;
-		p2=p3;
-		p3=p3->link;
-		p2->link=p1;
-	}
-	start=p2;
-}
-/*
-void sort(){
-    struct node *tmp;
-    if(start==NULL){
-        printf("Empty list\n");
+void insertInSorted(int data) {
+    struct node *tmp, *q;
+    if (start == NULL || start->info >= data) {
+        addAtBeg(data);
         return;
     }
-    tmp = start;
-
+    q = start;
+    while (q->link != NULL && q->link->info < data) {
+        q = q->link;
+    }
+    tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->info = data;
+    tmp->link = q->link;
+    q->link = tmp;
 }
-*/
-int main(){
 
-    int choice,n;
+void rev() {
+    struct node *p1, *p2, *p3;
+    if (start == NULL || start->link == NULL) return;
 
-    while (1)
-    {
-        // printf("\ni) Insert an element  before another  element  in the existing  list");
-        // printf("\nii) Insert an element  after another  element  in the  existing  list");
-        // printf("\niii) Delete a given element  from  the  list");
-        // printf("\niv) Traverse the list");
-        // printf("\nv) Reverse the list");
-        // printf("\nvi) Sort the  list");
-        // printf("\nvii) Delete every alternate node in the  list");
-        // printf("\nviii) Insert an element  in a sorted list such  that the  order is maintained.");
+    p1 = start;
+    p2 = p1->link;
+    p1->link = NULL;
 
-        printf("\n0. Exit\n1. Insert at end\n2. Insert at start\n3. Delete a given no\n4. Display\n5. Delete alternate nodes\n6. Insert element in sorted list\n7. Reverse");
+    while (p2 != NULL) {
+        p3 = p2->link;
+        p2->link = p1;
+        p1 = p2;
+        p2 = p3;
+    }
+    start = p1;
+}
+
+// Function to insert a node into the sorted part of the list
+void sortedInsert(struct node** sortedHead, struct node* newNode) {
+    struct node* current;
+    if (*sortedHead == NULL || (*sortedHead)->info >= newNode->info) {
+        newNode->link = *sortedHead;
+        *sortedHead = newNode;
+    } else {
+        current = *sortedHead;
+        while (current->link != NULL && current->link->info < newNode->info) {
+            current = current->link;
+        }
+        newNode->link = current->link;
+        current->link = newNode;
+    }
+}
+
+// Function to sort the list using insertion sort
+void sort() {
+    struct node *sorted = NULL;
+    struct node *current = start;
+    struct node *nextNode;
+
+    while (current != NULL) {
+        nextNode = current->link;  // Store next node
+        current->link = NULL;      // Isolate current node
+        sortedInsert(&sorted, current);  // Insert current node into sorted list
+        current = nextNode;        // Move to the next node
+    }
+    start = sorted;  // Update start to point to the new sorted list
+}
+
+int main() {
+    int choice, n;
+
+    while (1) {
+        printf("\n0. Exit\n1. Insert at end\n2. Insert at start\n3. Delete a given no\n4. Display\n5. Delete alternate nodes\n6. Insert element in sorted list\n7. Reverse\n8. Sort");
 
         printf("\nEnter Choice: ");
         scanf(" %d", &choice);
 
-        switch (choice)
-        {
+        switch (choice) {
         case 0:
             return 0;
         case 1:
             printf("Data: ");
-            scanf("%d",&n);
+            scanf("%d", &n);
             addAtEnd(n);
             break;
         case 2:
             printf("Data: ");
-            scanf("%d",&n);
+            scanf("%d", &n);
             addAtBeg(n);
             break;
         case 3:
             printf("Data: ");
-            scanf("%d",&n);
-            del(n);
+            scanf("%d", &n);
+            if (!del(n)) {
+                printf("Element %d not found\n", n);
+            }
             break;
         case 4:
             printf("List: ");
@@ -240,11 +184,14 @@ int main(){
             break;
         case 6:
             printf("Data: ");
-            scanf("%d",&n);
+            scanf("%d", &n);
             insertInSorted(n);
             break;
         case 7:
             rev();
+            break;
+        case 8:
+            sort();
             break;
         default:
             printf("Wrong choice, try again!\n");
